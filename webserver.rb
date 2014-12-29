138e9acdcb285c183a0dec8e7ad9b01b5a0378a2
CentOS <yangmiemie@localhost.localdomain>
2014-12-29 02:27:11 -0800

    add connection for socket

--- webserver.rb
+++ webserver.rb
@@ -8,16 +8,36 @@ class Webserver
 	def start
 		loop do
 			socket = @server.accept
-			data = socket.readpartial(1024)
-			puts data
-
-			socket.write "HTTP/1.1 200 OK\r\n"
-			socket.write "\r\n"
-			socket.write "hello\n"
-
-			socket.close
+			connection = Connection.new socket
+			connection.process
 		end
 	end
+
+	class Connection 
+		def initialize(socket)
+			@socket = socket
+		end
+
+		def process
+			data = @socket.readpartial(1024)
+			puts data
+			send_response
+		end
+
+		def send_response
+			@socket.write "HTTP/1.1 200 OK\r\n"
+			@socket.write "\r\n"
+			@socket.write "hello\n"
+		
+			close
+		end
+
+		def close
+			@socket.close
+		end
+
+	end
+
 end
 
 server = Webserver.new 3000
