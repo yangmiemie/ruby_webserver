fd1ed3128eb76ca1fec678df2acc62371927a813
CentOS <yangmiemie@localhost.localdomain>
2014-12-29 03:22:26 -0800

    add rack for webserver

--- webserver.rb
+++ webserver.rb
@@ -2,22 +2,24 @@ require 'socket'
 require 'http/parser'
 
 class Webserver
-	def initialize(port) 
+	def initialize(port, app) 
 		@server = TCPServer.new port
+		@app = app
 	end
 
 	def start
 		loop do
 			socket = @server.accept
-			connection = Connection.new socket
+			connection = Connection.new socket, @app
 			connection.process
 		end
 	end
 
 	class Connection 
-		def initialize(socket)
+		def initialize(socket, app)
 			@socket = socket
 			@parser = Http::Parser.new self
+			@app = app
 		end
 
 		def process
@@ -29,15 +31,36 @@ class Webserver
 
 		def on_message_complete
 			puts "#{@parser.http_method} #{@parser.request_url}"
-			# puts "#{@parser.http_method} #{@parser.request_path}"
 			puts " #{@parser.headers}" 
-			send_response
+
+			env = {}
+			env['PATH_INFO'] = @parser.request_url
+			env['REQUEST_METHOD'] = @parser.http_method
+
+			@parser.headers.each_pair do |name, value|
+				name = 'HTTP_' + name.upcase.tr('-', '_')
+				env[name] = value
+			end
+
+			send_response env
 		end
 
-		def send_response
-			@socket.write "HTTP/1.1 200 OK\r\n"
+		REASONS = {
+			200 => 'OK',
+			404 => 'Not Found'
+		}
+
+		def send_response(env)
+			status, header, body = @app.call env
+
+			@socket.write "HTTP/1.1 #{status} #{REASONS[status]}\r\n"
+			header.each_pair do |name, value|
+				@socket.write "#{name}:#{value}\r\n"
+			end
 			@socket.write "\r\n"
-			@socket.write "hello\n"
+			body.each do |message|
+				@socket.write message
+			end
 		
 			close
 		end
@@ -45,10 +68,25 @@ class Webserver
 		def close
 			@socket.close
 		end
+	end
 
+	class Builder
+		attr_reader :app
+
+		def run(app)
+			@app = app
+		end
+
+		def self.process(file)
+			content = File.read(file)
+			builder = self.new
+			builder.instance_eval content
+			builder.app
+		end
 	end
 
 end
 
-server = Webserver.new 3000
+app = Webserver::Builder.process('config.ru')
+server = Webserver.new 3000, app
 server.start
